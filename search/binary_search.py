a95a8ec622ad3af62d5ec954aea4e6bdde22a11d
ECcentricOG <umeshunde003@gmail.com>
2026-02-25 20:07:49 +0530

    binary search is added and leetcode 704 is solved

---
+++ search/binary_search.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+def search(nums:List[int], target:int) -> int:
+    if not nums:
+        return -1
+
+    start = 0
+    end = len(nums) - 1
+
+    while start <= end:
+        mid = start + (end - start) // 2
+        if nums[mid] == target:
+            return mid
+        elif target > nums[mid]:
+            start = mid + 1
+        else:
+            end = mid - 1
+    
+    return -1
