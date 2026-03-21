9e3c4b97882b544c4b1258e9516e25dca3935431
ECcentricOG <umeshunde003@gmail.com>
2026-03-21 11:34:08 +0530

    insertion sort is revised

--- sort/insertion_sort.py
+++ sort/insertion_sort.py
@@ -1,13 +1,14 @@
 from typing import List
 
 
-def sort(nums: List[int]) -> List:
+def sort(nums: List[int]) -> List[int]:
     n = len(nums)
 
     for i in range(1, n):
         ele = nums[i]
         j = i - 1
-        while j >= 0 and nums[j] < ele:
+        
+        while j >= 0 and ele < nums[j]:
             nums[j + 1] = nums[j]
             j -= 1
 
