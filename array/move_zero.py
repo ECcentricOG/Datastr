bfaadeea9201dd4ecabd5f343fa3ca6731146e9c
ECcentricOG <umeshunde003@gmail.com>
2026-03-21 11:42:44 +0530

    move zeros is revised

--- array/move_zero.py
+++ array/move_zero.py
@@ -2,11 +2,11 @@ from typing import List
 
 def move_zeros(nums:List[int]) -> None:
     n = len(nums)
-    show = 0
+    slow = 0
 
     for fast in range(n):
-        if nums[fast] != 0 and nums[show] == 0:
-            nums[fast], nums[show] = nums[show], nums[fast]
+        if nums[slow] == 0 and nums[fast] != 0:
+            nums[slow], nums[fast] = nums[fast], nums[slow]
 
-        if nums[show] != 0:
-            show += 1
+        if nums[slow] != 0:
+            slow += 1
