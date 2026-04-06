d55cfcdc59fb24344b7b59637f87615c36c77408
ECcentricOG <umeshunde003@gmail.com>
2026-04-06 15:17:26 +0530

    delete node is added in the linked list

--- linked_list/list_node.py
+++ linked_list/list_node.py
@@ -1,4 +1,8 @@
-class ListNode:
-    def __init__(self, val=0, next=None) -> None:
-        self.val = val
-        self.next = next
+from typing import Generic, Optional, TypeVar
+
+T = TypeVar("T")
+
+class ListNode(Generic[T]):
+    def __init__(self, val=None, next=None) -> None:
+        self.val:Optional[T] = val
+        self.next: Optional[ListNode] = next
